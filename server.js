9be20caf7c8042e4c87f47eccc2167cdb75f130e
Justin Ochoa <just.och92@gmail.com>
2022-07-03 13:08:22 -0400

    set up backend

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const workoutRoutes = require("./routes/workouts");
+require("dotenv").config();
+
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/workouts", workoutRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`connected to db and listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
